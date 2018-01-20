2bafffd7cb153a6d5416e58729e9c4209c634a9f
Corey Henderson <corman@cormander.com>
2018-01-20 14:25:24 -0700

    pass ENV header name with health check

--- poke.js
+++ poke.js
@@ -1,12 +1,14 @@
 var http = require("http");
 
+var ip_header = (process.env.CLIENT_IP_HEADER?process.env.CLIENT_IP_HEADER:"x-client-ip");
+
 var options = {  
    host : 'localhost',
    port : '8080',
    path: '/poke',
    timeout : 4500,
    headers: {
-     "cf-connecting-ip": "127.0.0.1"
+     [ip_header]: "127.0.0.1",
    },
 };
 
